97486a42044939e955e1b47cac99ad786822c885
Bijaya Giri <bijayagiri@Bijayas-MacBook-Pro.local>
2022-10-24 17:34:23 +0545

    new

--- server/index.js
+++ server/index.js
@@ -11,5 +11,16 @@ app.use(cors());
 const posts =require('/routes/api/posts');
 app.use('/api/posts',posts);
 
+
+
+//Handle Production...
+if(process.env.NODE_ENV==='production' ){
+    //static folder .
+    app.use(express.static(__dirname+'/public'));
+    //Handle SPA>....
+
+    app.get(/.*/,(req,res)=>res.sendFile(__dirname+ '/public/index.html'));
+}
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`server started on port 5000 ${port}`));
